5d4ed406bc16ee1cbd0b0985c4eaf96b83a8f6f4
Amanibhavam <iam@defn.sh>
2020-05-05 14:35:41 -0700

    easier style strings

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -68,11 +68,13 @@
 (def highlighted (atom #{}))        ; cells that are highlighted
 
 (def button-style
-  "inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs
+  "
+  inline-flex items-center px-2.5 py-1.5 border border-transparent text-xs
   leading-4 font-medium rounded text-indigo-700 bg-indigo-100
   hover:bg-indigo-50 focus:outline-none focus:border-indigo-300
   focus:shadow-outline-indigo active:bg-indigo-200 transition ease-in-out
-  duration-150")
+  duration-150
+  ")
 
 ; A gameboard is a grid of cells, each uniquely identified, but two cells will
 ; have the same number and colors.  The game is won when all cells have been
