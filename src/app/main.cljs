c8609c4372f83837e3046c8f858180da82e34b56
Amanibhavam <iam@defn.sh>
2020-05-06 01:14:37 -0700

    use a button group

--- src/app/main.cljs
+++ src/app/main.cljs
@@ -181,17 +181,32 @@
                                (fn [idx row] ^{:key idx} [board-row row])
                                (partition 4 cells))]]]
          ; the buttons
+         [:span {:class "relative z-0 inline-flex shadow-sm"}
           [:button {:type "button" 
                     :on-click #(new-game)
                     :href "#"
-                    :class (str button-style)} "restart"]
-
-          " " 
+                    :class "
+                           relative inline-flex items-center px-4 py-2
+                           rounded-l-md border border-gray-300 bg-white text-sm
+                           leading-5 font-medium text-gray-700
+                           hover:text-gray-500 focus:z-10 focus:outline-none
+                           focus:border-blue-300 focus:shadow-outline-blue
+                           active:bg-gray-100 active:text-gray-700 transition
+                           ease-in-out duration-150"}
+           "Restart"]
 
           [:button {:type "button"
                     :on-click #(win-game)
                     :href "#"
-                    :class (str button-style)} "cheat"]
+                    :class "
+                           -ml-px relative inline-flex items-center px-4 py-2
+                           rounded-r-md border border-gray-300 bg-white text-sm
+                           leading-5 font-medium text-gray-700
+                           hover:text-gray-500 focus:z-10 focus:outline-none
+                           focus:border-blue-300 focus:shadow-outline-blue
+                           active:bg-gray-100 active:text-gray-700 transition
+                           ease-in-out duration-150"}
+           "Cheat"]]
 
          ]]
 
